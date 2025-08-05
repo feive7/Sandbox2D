b95e181d5db347e8c8d52490a91999bb0b5180ae
feive7 <feive7@gmail.com>
2025-08-04 17:44:07 -0700

    DrawBody updated

--- Box2D/Box2D.cpp
+++ Box2D/Box2D.cpp
@@ -4,24 +4,41 @@
 
 void DrawBody(b2BodyId id, Color color) {
     // Compute AABB
-    b2ShapeId shapes[1]; // Only one shape for now
-    b2Body_GetShapes(id, shapes, 1);
+    b2ShapeId shapes[10];
+    b2Body_GetShapes(id, shapes, 10);
 
-    b2ShapeId shape = shapes[0]; // Shape
-    b2Polygon poly = b2Shape_GetPolygon(shape); // Polygon
+    for (int i = 0; i < b2Body_GetShapeCount(id); i++) {
+        b2ShapeId shape = shapes[i]; // Shape
+        int shapeType = b2Shape_GetType(shape);
 
-    // Get Transformation
-    b2Transform transformation = b2Body_GetTransform(id);
+        switch (shapeType) {
+        case b2_polygonShape: {
+            b2Polygon poly = b2Shape_GetPolygon(shape); // Polygon
 
-    b2Transform unrotated;
-    unrotated.p = transformation.p;
-    unrotated.q = b2Rot_identity;
+            // Get Transformation
+            b2Transform transformation = b2Body_GetTransform(id);
 
-    b2AABB aabb = b2ComputePolygonAABB(&poly, unrotated);
-    b2Vec2 extents = b2AABB_Extents(aabb);
-    b2Vec2 position = b2Body_GetPosition(id); // Get position of dynamic body
-    b2Rot rotation = b2Body_GetRotation(id); // Get rotation of dynamic body
-    DrawRectanglePro({ position.x, - position.y, 2 * extents.x, 2 * extents.y }, { extents.x, extents.y }, - b2Rot_GetAngle(rotation) * RAD2DEG, color); // Draw ground
+            b2Transform unrotated;
+            unrotated.p = transformation.p;
+            unrotated.q = b2Rot_identity;
+
+            b2AABB aabb = b2ComputePolygonAABB(&poly, unrotated);
+            b2Vec2 extents = b2AABB_Extents(aabb);
+            b2Vec2 position = b2Body_GetPosition(id); // Get position of dynamic body
+            b2Rot rotation = b2Body_GetRotation(id); // Get rotation of dynamic body
+            DrawRectanglePro({ position.x, position.y, 2 * extents.x, 2 * extents.y }, { extents.x, extents.y }, b2Rot_GetAngle(rotation) * RAD2DEG, color); // Draw ground
+            break;
+        }
+        case b2_circleShape: {
+            b2Circle circle = b2Shape_GetCircle(shape);
+            b2Transform transformation = b2Body_GetTransform(id);
+            b2Vec2 position = b2Body_GetPosition(id); // Get position of dynamic body
+            b2Rot rotation = b2Body_GetRotation(id); // Get rotation of dynamic body
+            DrawCircleV({ position.x, position.y }, circle.radius, color); // Draw circle
+            break;
+        }
+        }
+    }
 }
 int main() {
     // Window Definition
@@ -33,7 +50,7 @@ int main() {
     // Viewport Definition
     Camera2D viewport;
     viewport.offset = { 400.0f, 225.0f };
-    viewport.rotation = 0.0f;
+    viewport.rotation = 180;
     viewport.target = { 0.0f,0.0f };
     viewport.zoom = 3.0f;
 
@@ -50,24 +67,43 @@ int main() {
 
     b2BodyId groundId = b2CreateBody(worldId, &groundBodyDef);
 
-    b2Polygon groundBox = b2MakeBox(50.0f, 10.0f);
+    b2Polygon groundBox = b2MakeBox(50.0f, 2.0f);
 
     b2ShapeDef groundShapeDef = b2DefaultShapeDef();
     b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
 
+	b2Body_SetTransform(groundId, { 0.0f, -10.0f }, b2MakeRot(PI / 16.0f)); // Set ground position
+
     // Dynamic Body Definition
-    b2BodyDef bodyDef = b2DefaultBodyDef();
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position = { 60.0f, 40.0f };
-    b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
+    b2BodyDef boxBodyDef = b2DefaultBodyDef();
+    boxBodyDef.type = b2_dynamicBody;
+    boxBodyDef.position = { 0.0f, 40.0f };
+    b2BodyId boxBodyId = b2CreateBody(worldId, &boxBodyDef);
 
     b2Polygon dynamicBox = b2MakeBox(20.0f, 10.0f);
 
-    b2ShapeDef shapeDef = b2DefaultShapeDef();
-    shapeDef.density = 1.0f;
-    shapeDef.material.friction = 0.3f;
+    b2ShapeDef boxShapeDef = b2DefaultShapeDef();
+    boxShapeDef.density = 1.0f;
+    boxShapeDef.material.friction = 0.3f;
 
-    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox); // Attaches shapeDef to dynamicBox
+    b2CreatePolygonShape(boxBodyId, &boxShapeDef, &dynamicBox);
+
+    // Circle Body Definition
+    b2BodyDef circleBodyDef = b2DefaultBodyDef();
+    circleBodyDef.type = b2_dynamicBody;
+    circleBodyDef.position = { 0.0f, 20.0f };
+    b2BodyId circleBodyId = b2CreateBody(worldId, &circleBodyDef);
+
+    b2Circle dynamicCircle;
+    dynamicCircle.center = { 0.0f,0.0f };
+    dynamicCircle.radius = 10.0f;
+
+    b2ShapeDef circleShapeDef = b2DefaultShapeDef();
+
+    b2CreateCircleShape(circleBodyId, &circleShapeDef, &dynamicCircle);
+
+	// Mouse Joint Definition
+
 
     // Simulation setup
     float timeStep = 1.0f / 60.0f; // 60Hz
@@ -75,13 +111,21 @@ int main() {
     int subStepCount = 4; // Run 4 simsteps each frame
 
     while (!WindowShouldClose()) {
+        // Handle inputs
+        float mwMove = GetMouseWheelMove();
+        if (mwMove) {
+            viewport.zoom *= pow(2.0,mwMove / 10.0f);
+        }
+		Vector2 mousePos = GetScreenToWorld2D(GetMousePosition(), viewport);
+        //b2Body_SetTransform(circleBodyId, { mousePos.x,mousePos.y }, b2Rot_identity);
+
         // Simulate
         float deltaTime = GetFrameTime();
         b2World_Step(worldId, deltaTime, subStepCount);
+        
 
-
-        b2Vec2 position = b2Body_GetPosition(bodyId); // Get position of dynamic body
-        b2Rot rotation = b2Body_GetRotation(bodyId); // Get rotation of dynamic body
+        b2Vec2 position = b2Body_GetPosition(boxBodyId); // Get position of dynamic body
+        b2Rot rotation = b2Body_GetRotation(boxBodyId); // Get rotation of dynamic body
 
         b2Vec2 groundPos = b2Body_GetPosition(groundId);
         b2Rot groundRot = b2Body_GetRotation(groundId);
@@ -89,7 +133,8 @@ int main() {
         ClearBackground(RAYWHITE);
         BeginMode2D(viewport);
 
-        DrawBody(bodyId, RED);
+        DrawBody(boxBodyId, RED);
+        DrawBody(circleBodyId, GREEN);
         DrawBody(groundId, BLUE);
 
         EndMode2D();
