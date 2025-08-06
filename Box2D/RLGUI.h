42fd07483ef7a0ad61b75eba395c4797786b8ae9
feive7 <feive7@gmail.com>
2025-08-06 15:44:36 -0700

    Added RLGUI

---
+++ Box2D/RLGUI.h
@@ -0,0 +1,37 @@
+#include <functional>
+#include <vector>
+#include <string>
+struct Button {
+	int x, y, width, height, fontSize;
+	Color bgColor;
+	Color fontColor;
+	std::string text;
+	int id;
+};
+class GUI {
+private:
+	std::vector<Button> buttons;
+public:
+	void addButton(const Button& btn) {
+		buttons.push_back(btn);
+	}
+	void draw() {
+		DrawRectangle(0, 0, 800, 800, { 255,255,255,120 });
+		for (Button btn : buttons) {
+			DrawRectangle(btn.x, btn.y, btn.width, btn.height, ColorBrightness(btn.bgColor, -0.2));
+			DrawRectangle(btn.x + 2, btn.y + 2, btn.width - 4, btn.height - 4, btn.bgColor);
+			int textLength = MeasureText(btn.text.c_str(), btn.fontSize);
+			DrawText(TextFormat("ID: %i", btn.id), btn.x + 2, btn.y + 2, 10, btn.fontColor);
+			DrawText(btn.text.c_str(), btn.x + (btn.width - textLength) / 2, btn.y + (btn.height - 20) / 2, btn.fontSize, btn.fontColor);
+		}
+	}
+	int getHovering(Vector2 mousePosition) {
+		for (Button btn : buttons) {
+			if (mousePosition.x >= btn.x && mousePosition.x <= btn.x + btn.width &&
+				mousePosition.y >= btn.y && mousePosition.y <= btn.y + btn.height) {
+				return btn.id;
+			}
+		}
+		return -1;
+	}
+};
