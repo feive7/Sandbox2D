021f4498a1e7398ff447200e2a9d9a46f1d0013b
feive7 <feive7@gmail.com>
2025-08-06 15:55:39 -0700

    Added labels

--- Box2D/RLGUI.h
+++ Box2D/RLGUI.h
@@ -1,6 +1,11 @@
 #include <functional>
 #include <vector>
 #include <string>
+struct Label {
+	int x, y, fontSize;
+	Color fontColor;
+	std::string text;
+};
 struct Button {
 	int x, y, width, height, fontSize;
 	Color bgColor;
@@ -10,10 +15,14 @@ struct Button {
 };
 class GUI {
 private:
+	std::vector<Label> labels;
 	std::vector<Button> buttons;
 public:
 	void addButton(const Button& btn) {
 		buttons.push_back(btn);
+	}
+	void addLabel(const Label& lbl) {
+		labels.push_back(lbl);
 	}
 	void draw() {
 		DrawRectangle(0, 0, 800, 800, { 255,255,255,120 });
@@ -23,6 +32,9 @@ public:
 			int textLength = MeasureText(btn.text.c_str(), btn.fontSize);
 			DrawText(TextFormat("ID: %i", btn.id), btn.x + 2, btn.y + 2, 10, btn.fontColor);
 			DrawText(btn.text.c_str(), btn.x + (btn.width - textLength) / 2, btn.y + (btn.height - 20) / 2, btn.fontSize, btn.fontColor);
+		}
+		for (Label lbl : labels) {
+			DrawText(lbl.text.c_str(), lbl.x, lbl.y, lbl.fontSize, lbl.fontColor);
 		}
 	}
 	int getHovering(Vector2 mousePosition) {
