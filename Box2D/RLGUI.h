4e54f7b8a19f5c2366fdf7452949155afb91c6fc
feive7 <feive7@gmail.com>
2025-08-06 20:10:15 -0700

    added adjusted position so the gui doesn't draw offscreen

--- Box2D/RLGUI.h
+++ Box2D/RLGUI.h
@@ -25,18 +25,24 @@ public:
 	void addLabel(const Label& lbl) {
 		labels.push_back(lbl);
 	}
+	Vector2 getAdjustedPosition() {
+		float guiX = fmin(GetScreenWidth() - (width + padding), x);
+		float guiY = fmin(GetScreenHeight() - (height + padding), y);
+		return { guiX, guiY };
+	}
 	void draw() {
-		DrawRectangle(x, y, padding + width, padding + height, { 255,255,255,120 });
-		DrawRectangleLines(x, y, padding + width, padding + height, BLACK);
+		Vector2 gui = getAdjustedPosition();
+		DrawRectangle(gui.x, gui.y, padding + width, padding + height, { 255,255,255,120 });
+		DrawRectangleLines(gui.x, gui.y, padding + width, padding + height, BLACK);
 		for (Button btn : buttons) {
-			DrawRectangle(padding + x + btn.x, padding + y + btn.y, btn.width, btn.height, ColorBrightness(btn.bgColor, -0.2));
-			DrawRectangle(padding + x + btn.x + 2, padding + y + btn.y + 2, btn.width - 4, btn.height - 4, btn.bgColor);
+			DrawRectangle(padding + gui.x + btn.x, padding + gui.y + btn.y, btn.width, btn.height, ColorBrightness(btn.bgColor, -0.2));
+			DrawRectangle(padding + gui.x + btn.x + 2, padding + gui.y + btn.y + 2, btn.width - 4, btn.height - 4, btn.bgColor);
 			int textLength = MeasureText(btn.text.c_str(), btn.fontSize);
-			DrawText(TextFormat("ID: %i", btn.id), x + btn.x + 2, y + btn.y + 2, 10, btn.fontColor);
-			DrawText(btn.text.c_str(), padding + x + btn.x + (btn.width - textLength) / 2, y + btn.y + (btn.height - 20) / 2, btn.fontSize, btn.fontColor);
+			DrawText(TextFormat("ID: %i", btn.id), padding + gui.x + btn.x + 2, padding + gui.y + btn.y + 2, 10, btn.fontColor);
+			DrawText(btn.text.c_str(), padding + gui.x + btn.x + (btn.width - textLength) / 2, gui.y + btn.y + (btn.height - 20) / 2, btn.fontSize, btn.fontColor);
 		}
 		for (Label lbl : labels) {
-			DrawText(lbl.text.c_str(), padding + x + lbl.x, y + lbl.y, lbl.fontSize, lbl.fontColor);
+			DrawText(lbl.text.c_str(), padding + gui.x + lbl.x, gui.y + lbl.y, lbl.fontSize, lbl.fontColor);
 		}
 	}
 	void sizeToFit() {
@@ -58,9 +64,10 @@ public:
 		height = maxHeight;
 	}
 	int getHovering(Vector2 mousePosition) {
+		Vector2 adjustedPosition = getAdjustedPosition();
 		for (Button btn : buttons) {
-			if (mousePosition.x >= padding + x + btn.x && mousePosition.x <= padding + x + btn.x + btn.width &&
-				mousePosition.y >= padding + y + btn.y && mousePosition.y <= padding + y + btn.y + btn.height) {
+			if (mousePosition.x >= padding + adjustedPosition.x + btn.x && mousePosition.x <= padding + adjustedPosition.x + btn.x + btn.width &&
+				mousePosition.y >= padding + adjustedPosition.y + btn.y && mousePosition.y <= padding + adjustedPosition.y + btn.y + btn.height) {
 				return btn.id;
 			}
 		}
